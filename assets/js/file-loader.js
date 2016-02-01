a324a7403053e45a9b3e9ac3584aca45ba69f2b4
Matthew Gamble <git@matthewgamble.net>
2016-02-01 19:46:11 +1100

    Don't use snake_case

--- assets/js/file-loader.js
+++ assets/js/file-loader.js
@@ -1,34 +1,34 @@
-function FileLoaderFactory(download_url, sysload_url)
+function FileLoaderFactory(downloadUrl, sysloadUrl)
 {
-    this.download_url = download_url;
-    this.sysload_url = sysload_url;
+    this.downloadUrl = downloadUrl;
+    this.sysloadUrl = sysloadUrl;
 }
 
 FileLoaderFactory.prototype = {
-    create: function(pathID, files, concurrency_limit) {
+    create: function(pathID, files, concurrencyLimit) {
         var fl;
-        if (concurrency_limit) {
-            fl = new FileLoader(pathID, files, this.download_url, concurrency_limit, this.sysload_url);
+        if (concurrencyLimit) {
+            fl = new FileLoader(pathID, files, this.downloadUrl, concurrencyLimit, this.sysloadUrl);
         } else {
-            fl = new FileLoader(pathID, files, this.download_url);
+            fl = new FileLoader(pathID, files, this.downloadUrl);
         }
         return fl;
     }
 };
 
-function FileLoader(pathID, files, download_url, concurrency_limit, sysload_url)
+function FileLoader(pathID, files, downloadUrl, concurrency_limit, sysloadUrl)
 {
     this.pathID = pathID;
     this.files = files;
-    this.download_url = download_url;
+    this.downloadUrl = downloadUrl;
 
     this.open_count = 0;
     this.orig_concurrency_limit = (!isNaN(parseInt(concurrency_limit)) && concurrency_limit > 2 ? concurrency_limit : 2);
     this.concurrency_limit = this.orig_concurrency_limit;
-    this.sysload_url = sysload_url;
+    this.sysloadUrl = sysloadUrl;
 
-    this.file_count = this.files.length;
-    this.processed_count = 0;
+    this.fileCount = this.files.length;
+    this.processedCount = 0;
 }
 
 FileLoader.prototype = {
@@ -38,9 +38,11 @@ FileLoader.prototype = {
         }
         jQuery(this).trigger("file_loader:start_load");
         var self = this;
-        if (this.sysload_url) {
+        if (this.sysloadUrl) {
             this._checkSysload();
         }
+        // TODO: Optimise to not have a billion timeouts happening at once
+        // Also should be able to use Array.prototype.forEach
         jQuery.each(this.files, function(index, value) {
             var waitFunc = function() {
                 if (self.open_count >= self.concurrency_limit) {
@@ -60,13 +62,13 @@ FileLoader.prototype = {
             method: "POST",
             data: {path: this.pathID, "filename": value},
             dataType: "native",
-            url: this.download_url,
+            url: this.downloadUrl,
             xhrFields: {
                 responseType: "blob"
             }
         }).done(function(data) {
             loadCallback(value, window.URL.createObjectURL(data));
-        }).fail(function(jq, textStatus, errorThrown) {
+        }).fail(function(jqXHR, textStatus, errorThrown) {
             var msg = "An error occurred while loading '" + value + "':\n" + textStatus;
             if (textStatus == "error") {
                 msg += "\n" + errorThrown;
@@ -74,8 +76,8 @@ FileLoader.prototype = {
             errorCallback(msg);
         }).always(function() {
             self.open_count--;
-            self.processed_count++;
-            if (self.processed_count === self.file_count) {
+            self.processedCount++;
+            if (self.processedCount === self.fileCount) {
                 jQuery(self).trigger("file_loader:finish_load");
             }
         });
@@ -83,7 +85,7 @@ FileLoader.prototype = {
 
     _checkSysload: function() {
         var self = this;
-        jQuery.ajax(this.sysload_url).done(function(data) {
+        jQuery.ajax(this.sysloadUrl).done(function(data) {
             if (isNaN(parseInt(data))) {
                 // Assume an error occurred, and automatically scale back the concurrency.
                 self.concurrency_limit = self.orig_concurrency_limit - 1;
@@ -100,7 +102,7 @@ FileLoader.prototype = {
                 }
             }
         }).always(function() {
-            if (self.processed_count < self.file_count) {
+            if (self.processedCount < self.fileCount) {
                 setTimeout(self._checkSysload.bind(self), 15 * 1000);
             }
         });
