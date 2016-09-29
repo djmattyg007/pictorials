8067d703f87b666837dfc8d632bf0a69cc5b09fd
Matthew Gamble <git@matthewgamble.net>
2016-09-29 23:02:59 +1000

    Fix more typos

--- helpers/checkimagetype.php
+++ helpers/checkimagetype.php
@@ -3,15 +3,15 @@
 /** @var string $filename */
 
 $allowedImageTypes = loadPicFile("helpers/imagetypes.php");
-$type = new Hoa\Mime\Mime(new Hoa\File\Read($filename));
+//$type = new Hoa\Mime\Mime(new Hoa\File\Read($filename));
 $originalExtension = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
 //$originalExtension = strtolower($type->getExtension());
 if (!in_array($originalExtension, $allowedImageTypes)) {
     sendError(400);
 }
-$mimeType = MimeType::extensionToMimeType($originalExtension);
+$mimeType = MrMime::extensionToMimeType($originalExtension);
 //$mimeType = $type->getMime();
-$actualMimeType = MimeType::resolveMimeType($filename);
+$actualMimeType = MrMime::resolveMimeType($filename);
 //$actualMimeType = HoaMimeWrapper::picGetMimeTypeFromFilename($filename);
 if ($actualMimeType !== $mimeType) {
     Logger::warning("error", "Invalid image found.", array("filename" => $filename, "expectedMimeType" => $mimeType, "actualMimeType" => $actualMimeType));
