f517cf755d20b07242abd9e63230939be6568948
Matthew Gamble <git@matthewgamble.net>
2017-07-04 18:35:35 +1000

    Add CLI command to view all users in database

---
+++ entry/_user/list.php
@@ -0,0 +1,29 @@
+<?php
+
+PicCLI::initGetopt(array("sort::"));
+$io = PicCLI::getIO();
+
+loadPicFile("classes/db.php");
+PicDB::initDB();
+
+$select = PicDB::newSelect();
+$select->cols(array("id", "username", "name"))
+    ->from("users");
+$sortOption = PicCLI::getGetopt("--sort");
+if ($sortOption === true || $sortOption === "username") {
+    $select->orderBy(array("username ASC"));
+} elseif ($sortOption === "name") {
+    $select->orderBy(array("name ASC"));
+}
+$select->orderBy(array("id ASC"));
+$rows = PicDB::fetch($select, "assoc");
+if (empty($rows)) {
+    $io->outln("No users have been created.");
+} else {
+    $highestId = max(array_keys($rows));
+    $idWidth = strlen((string) $highestId);
+    foreach ($rows as $id => $data) {
+        $io->out(sprintf("<<blue>>%s<<reset>> ", str_pad($id, $idWidth)));
+        $io->outln(sprintf('%1$s - %2$s', $data["username"], $data["name"]));
+    }
+}
--- entry/user.php
+++ entry/user.php
@@ -13,7 +13,7 @@ require(BASE_PATH . "main/bootstrap.php");
 
 loadPicFile("classes/cli.php");
 try {
-    $command = PicCLI::initCommandCLI(array("create", "update", "delete", "view"));
+    $command = PicCLI::initCommandCLI(array("create", "update", "delete", "view", "list"));
 } catch (Exception $e) {
     PicCLI::getIO()->errln($e->getMessage());
     exit(PicCLI::EXIT_USAGE);
