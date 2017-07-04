57bbff4f6f0ca55346698a135deb7dc8000f6118
Matthew Gamble <git@matthewgamble.net>
2017-07-04 18:18:42 +1000

    Add another version check, fix some output in upgrade tool

--- entry/_system/upgrade.php
+++ entry/_system/upgrade.php
@@ -4,8 +4,14 @@ $io = PicCLI::getIO();
 loadPicFile("classes/db.php");
 $conn = PicDB::initDB();
 $oldVersion = PicDB::getSystemVal("version");
+if (version_compare($oldVersion, VERSION, ">") === true) {
+    $io->errln("It looks like you've attempted to downgrade Pictorials to an earlier version.");
+    $io->errln("This is not a supported action. In future, you should back up your database");
+    $io->errln("so you can roll back to an earlier version if an upgrade fails.");
+    exit(1); // TOOD: Find an appropriate exit code
+}
 if (version_compare($oldVersion, VERSION, "=") === true) {
-    PicCLI::warn("<<yellow>>You are already on the latest version of the software.<<reset>>");
+    PicCLI::warn("You are already on the latest version of the software.");
     exit(PicCLI::EXIT_SUCCESS);
 }
 
