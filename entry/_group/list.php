0c16a4acfcd86b3346981fee4eef099db8b5964c
Matthew Gamble <git@matthewgamble.net>
2017-07-05 22:18:17 +1000

    Code style fix

--- entry/_group/list.php
+++ entry/_group/list.php
@@ -18,8 +18,8 @@ $rows = PicDB::fetch($select, "assoc");
 if (empty($rows)) {
     $io->outln("No groups have been created.");
 } else {
-    $highestId = max(array_keys($rows));
-    $idWidth = strlen((string) $highestId);
+    $highestID = max(array_keys($rows));
+    $idWidth = strlen((string) $highestID);
     foreach ($rows as $id => $data) {
         $io->out(sprintf("<<blue>>%s<<reset>> ", str_pad($id, $idWidth)));
         $io->outln(sprintf('%1$s', $data["name"]));
