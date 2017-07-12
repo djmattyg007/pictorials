b853587b26f7ce96e881772799f0c26b6573d862
Matthew Gamble <git@matthewgamble.net>
2017-07-13 00:32:51 +1000

    Use slightly worse quality for small images

--- modes/loadimage.php
+++ modes/loadimage.php
@@ -10,10 +10,11 @@ list($normalisedExtension, $mimeType) = loadPicFile("helpers/checkimagetype.php"
 
 $imageSizes = loadPicFile("conf/app.json")["image_sizes"];
 if (empty($_POST["size"]) || !in_array($_POST["size"], array_keys($imageSizes))) {
-    $imageSize = $imageSizes["medium"];
+    $imageSizeID = "medium";
 } else {
-    $imageSize = $imageSizes[$_POST["size"]];
+    $imageSizeID = $_POST["size"];
 }
+$imageSize = $imageSizes[$imageSizeID];
 
 $path = Access::getCurrentPath();
 
@@ -21,7 +22,7 @@ $image = PicImage::open($fullFilename);
 $image->cropResize($imageSize["width"], $imageSize["height"]);
 $image->fixOrientation();
 
-$imageData = $image->cacheData($normalisedExtension, 95);
+$imageData = $image->cacheData($normalisedExtension, $imageSizeID === "small" ? 85 : 95);
 
 header("Content-type: $mimeType");
 
