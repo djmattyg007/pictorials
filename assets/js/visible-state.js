73d800315ebc1442676fcf84c870c0cf15121c9a
Matthew Gamble <git@matthewgamble.net>
2017-07-15 13:48:24 +1000

    In-progress updates to visible-state plugin, file manager toolbox

--- assets/js/visible-state.js
+++ assets/js/visible-state.js
@@ -1,10 +1,12 @@
-jQuery(document).on("click", "[data-toggle='visible-state']", function() {
-    var target = document.getElementById(this.dataset.target);
-    if (target.dataset.visibleState === "open") {
-        target.dataset.visibleState = "closed";
-        this.setAttribute("aria-expanded", "false");
-    } else {
-        target.dataset.visibleState = "open";
-        this.setAttribute("aria-expanded", "true");
-    }
-});
+(function() {
+    var update = function(open, el) {
+        el.dataset.visibleState = (open === true ? "open" : "closed");
+    };
+    jQuery(document).on("click", "[data-toggle='visible-state']", function() {
+        // If currently open, close. Otherwise, open.
+        var open = this.getAttribute("aria-expanded") === "true" ? false : true;
+        var targets = document.querySelectorAll(this.dataset.target);
+        Array.prototype.forEach.call(targets, update.bind(null, open));
+        this.setAttribute("aria-expanded", String(open));
+    });
+})();
